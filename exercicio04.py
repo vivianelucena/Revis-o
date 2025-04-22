b6a308179e352d4ce570f9baadbec8d4b3a77554
Viviane <vivianedelucenasilva@gmail.com>
2025-04-22 09:31:15 -0300

    Arquivos

---
+++ exercicio04.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f'O antecessor do número {numero} é {antecessor} e o seu sucessor é {sucessor}!')
---
+++ exercicio05.py
@@ -0,0 +1,5 @@
+salario = float(input('Digite seu salário: '))
+minimo = float(input('Digite o salario mínimo atualmente: '))
+divisao = salario/minimo
+
+print(f'O salário é de R${salario}, que equivale a {divisao} salários mínimos!')
