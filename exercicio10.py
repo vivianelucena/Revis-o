316512a49d51f5b2f029fcb1ce20be709e203bbe
Viviane <vivianedelucenasilva@gmail.com>
2025-04-22 11:25:41 -0300

    exercicio 10 e 11

---
+++ exercicio10.py
@@ -0,0 +1,3 @@
+for a in range(100, 1, -1):
+    if a % 2 == 0:
+        print(a, end= ' ')
---
+++ exercicio11.py
@@ -0,0 +1,6 @@
+cont = 100
+
+while cont >= 1:
+    cont = cont - 1
+    if cont % 2 == 0:
+        print(cont)
